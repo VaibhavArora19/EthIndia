1540f78d3a4be1c819913ef6647ee6ecdf070fc1
Aman-Mandal <002amankumar@gmail.com>
2023-12-08 23:39:29 +0530

    Added layout and navbar

---
+++ frontend/components/UI/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <nav className='w-full py-3 justify-between flex px-10 text-white fixed font-Avenir items-center bg-inherit  z-10'>
+      <p>SIT</p>
+      <button className='bg-[#292929] hover:bg-[#333333] text-sm py-2 px-6 rounded-md'>
+        Connect Wallet
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
