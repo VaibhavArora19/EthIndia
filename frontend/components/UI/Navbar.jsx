7185e2b0005b740ecb8700802f19c60e3b7c55db
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-12-09 01:23:52 +0530

    integrated web3 auth

--- frontend/components/UI/Navbar.jsx
+++ frontend/components/UI/Navbar.jsx
@@ -1,10 +1,13 @@
+import HomeButton from "./HomeButton";
+
 const Navbar = () => {
   return (
-    <nav className='w-full py-3 justify-between flex px-10 text-white fixed font-Avenir items-center bg-inherit  z-10'>
+    <nav className="w-full py-3 justify-between flex px-10 text-white fixed font-Avenir items-center bg-inherit  z-10">
       <p>SIT</p>
-      <button className='bg-[#292929] hover:bg-[#333333] text-sm py-2 px-6 rounded-md'>
+      {/* <button className='bg-[#292929] hover:bg-[#333333] text-sm py-2 px-6 rounded-md'>
         Connect Wallet
-      </button>
+      </button> */}
+      <HomeButton />
     </nav>
   );
 };
