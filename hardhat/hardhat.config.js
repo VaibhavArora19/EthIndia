f54cd520a56a9ec3b706b34889b64d415809cbcf
dinesh11515 <dineshaitham2@gmail.com>
2023-12-09 02:53:40 +0530

    added aadhaar code

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,6 +1,36 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-dependency-compiler");
+require("dotenv").config();
 
+const DEPLOYER_PRIVATE_KEY = process.env.DEPLOYER_PRIVATE_KEY;
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.20",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.20",
+      },
+    ],
+  },
+  dependencyCompiler: {
+    paths: ["anon-aadhaar-contracts/contracts/Verifier.sol"],
+  },
+
+  networks: {
+    zkevm: {
+      // If not set, you can get your own Alchemy API key at https://dashboard.alchemyapi.io or https://infura.io
+      url: "https://rpc.public.zkevm-test.net	",
+      accounts: [DEPLOYER_PRIVATE_KEY],
+    },
+    goerli: {
+      // If not set, you can get your own Alchemy API key at https://dashboard.alchemyapi.io or https://infura.io
+      url: process.env.GOERLI_RPC_URL ?? "",
+      accounts: [DEPLOYER_PRIVATE_KEY],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      polygonZkEVMTestnet: "DGGT1IJ7YX5CJ39I2U3KRGW1T45A4BYWW2",
+    },
+  },
 };
