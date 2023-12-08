a3e4a54874e3247c01920583eb72f96b46056fcc
Aman-Mandal <002amankumar@gmail.com>
2023-12-09 02:37:13 +0530

    Merged conflicts

--- frontend/components/UI/Layout.jsx
+++ frontend/components/UI/Layout.jsx
@@ -4,7 +4,7 @@ const Layout = ({ children }) => {
   return (
     <div className='bg-[url("/bg.png")] h-screen w-screen  font-Avenir'>
       <Navbar />
-      <div className='flex justify-center pt-36 h-full'>{children}</div>
+      <div>{children}</div>
     </div>
   );
 };
---
+++ frontend/components/UI/PieChart.jsx
@@ -0,0 +1,55 @@
+import { Pie } from 'react-chartjs-2';
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+const data = {
+  labels: ['Uniswap', 'Chainlink', 'BNB', 'SOL', 'Matic', 'Bitcoin'],
+  datasets: [
+    {
+      label: '% of holdings',
+      data: [17, 3, 3, 30, 10, 30],
+      backgroundColor: [
+        'rgba(255, 99, 132, 0.2)',
+        'rgba(54, 162, 235, 0.2)',
+        'rgba(255, 206, 86, 0.2)',
+        'rgba(75, 192, 192, 0.2)',
+        'rgba(153, 102, 255, 0.2)',
+        'rgba(255, 159, 64, 0.2)',
+      ],
+      borderColor: [
+        'rgba(255, 99, 132, 1)',
+        'rgba(54, 162, 235, 1)',
+        'rgba(255, 206, 86, 1)',
+        'rgba(75, 192, 192, 1)',
+        'rgba(153, 102, 255, 1)',
+        'rgba(255, 159, 64, 1)',
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: 'top',
+    },
+    title: {
+      display: true,
+      text: 'Pie Chart Example',
+    },
+  },
+};
+
+const PieChart = () => {
+  return (
+    <Pie
+      data={data}
+      options={options}
+    />
+  );
+};
+
+export default PieChart;
