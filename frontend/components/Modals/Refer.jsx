7b016c3b2c2935d15f83dd50a226d00d4562e0bf
dinesh11515 <dineshaitham2@gmail.com>
2023-12-09 23:43:46 +0530

    added get refers

--- frontend/components/Modals/Refer.jsx
+++ frontend/components/Modals/Refer.jsx
@@ -1,22 +1,38 @@
-import React, { useState } from 'react';
-import Backdrop from '../UI/Backdrop';
-import { FaLink } from 'react-icons/fa6';
-import { useAccount } from 'wagmi';
-import { exportCallDataGroth16FromPCD } from 'anon-aadhaar-pcd';
-import { writeContract, waitForTransaction } from '@wagmi/core';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import { referralAbi, referralContract } from '@/constants';
+import React, { useState } from "react";
+import Backdrop from "../UI/Backdrop";
+import { FaLink } from "react-icons/fa6";
+import { useAccount } from "wagmi";
+import { exportCallDataGroth16FromPCD } from "anon-aadhaar-pcd";
+import { writeContract, waitForTransaction, readContract } from "@wagmi/core";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { referralAbi, referralContract } from "@/constants";
 
 import {
   LogInWithAnonAadhaar,
   useAnonAadhaar,
   AnonAadhaarProof,
-} from 'anon-aadhaar-react';
+} from "anon-aadhaar-react";
 const Refer = ({ onClose }) => {
   const { address } = useAccount();
   const [anonAadhaar] = useAnonAadhaar();
   const [register, setRegister] = useState(false);
+
+  const getRefer = async () => {
+    try {
+      const data = await readContract({
+        address: referralContract,
+        abi: referralAbi,
+        functionName: "referrers",
+        args: [address],
+      });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getRefer();
 
   const registerReferrer = async () => {
     try {
@@ -26,13 +42,13 @@ const Refer = ({ onClose }) => {
       const { hash } = await writeContract({
         address: referralContract,
         abi: referralAbi,
-        functionName: 'registerReferrer',
+        functionName: "registerReferrer",
         args: [a, b, c, Input],
       });
       await waitForTransaction({ hash });
-      console.log('xxx', hash);
+      console.log("xxx", hash);
       setRegister(true);
-      toast.success('Registered Successfully');
+      toast.success("Registered Successfully");
     } catch (e) {
       console.log(e);
     }
@@ -41,16 +57,18 @@ const Refer = ({ onClose }) => {
   return (
     <>
       <Backdrop onClose={onClose} />
-      <div className='bg-[#0F0F0F] w-[500px] fixed top-[50%] left-[50%] text-white -translate-x-[50%] -translate-y-[50%] z-30 p-6 rounded-md'>
-        <p className='text-2xl font-semibold mb-1'>Refer & Earn</p>
-        <p className='mb-5'>
+      <div className="bg-[#0F0F0F] w-[500px] fixed top-[50%] left-[50%] text-white -translate-x-[50%] -translate-y-[50%] z-30 p-6 rounded-md">
+        <p className="text-2xl font-semibold mb-1">Refer & Earn</p>
+        <p className="mb-5">
           Only people with the aadhar card can use this Referal system signing
           up with ANON Aadhar.
         </p>
 
-        <p className='text-sm font-semibold text-left mb-5'>Note: 10 rSIT = 1 SIT</p>
+        <p className="text-sm font-semibold text-left mb-5">
+          Note: 10 rSIT = 1 SIT
+        </p>
         {/** Hide once logged in */}
-        {anonAadhaar.status != 'logged-in' ? (
+        {anonAadhaar.status != "logged-in" ? (
           <LogInWithAnonAadhaar />
         ) : register ? (
           <button
@@ -58,9 +76,10 @@ const Refer = ({ onClose }) => {
               navigator.clipboard.writeText(
                 `${window.location.origin}/claim/${address}`
               );
-              toast.success('copied');
+              toast.success("copied");
             }}
-            className='w-full flex items-center gap-2 justify-center text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40'>
+            className="w-full flex items-center gap-2 justify-center text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40"
+          >
             <FaLink size={20} />
             <p>Copy Link</p>
           </button>
@@ -68,8 +87,9 @@ const Refer = ({ onClose }) => {
           <div>
             {/* Show After login with Aadhar */}
             <button
-              className='w-full text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40'
-              onClick={registerReferrer}>
+              className="w-full text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40"
+              onClick={registerReferrer}
+            >
               Register
             </button>
           </div>
--- frontend/pages/claim/[address].jsx
+++ frontend/pages/claim/[address].jsx
@@ -1,16 +1,35 @@
-import { LogInWithAnonAadhaar, useAnonAadhaar } from 'anon-aadhaar-react';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React, { useState } from 'react';
-import { referralAbi, referralContract } from '@/constants';
-import { writeContract, waitForTransaction } from '@wagmi/core';
-import { exportCallDataGroth16FromPCD } from 'anon-aadhaar-pcd';
+import { LogInWithAnonAadhaar, useAnonAadhaar } from "anon-aadhaar-react";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { referralAbi, referralContract } from "@/constants";
+import { writeContract, waitForTransaction, readContract } from "@wagmi/core";
+import { exportCallDataGroth16FromPCD } from "anon-aadhaar-pcd";
+import { useAccount } from "wagmi";
 
 const Claim = () => {
+  const { address: referred } = useAccount();
+
   const router = useRouter();
   const [anonAadhaar] = useAnonAadhaar();
   const [claimed, setClaimed] = useState(false);
   const { address } = router.query;
+
+  const getRefered = async () => {
+    try {
+      const data = await readContract({
+        address: referralContract,
+        abi: referralAbi,
+        functionName: "referred",
+        args: [referred],
+      });
+      console.log(data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  getRefered();
 
   const verifyReferrer = async () => {
     try {
@@ -20,64 +39,66 @@ const Claim = () => {
       const { hash } = await writeContract({
         address: referralContract,
         abi: referralAbi,
-        functionName: 'verifyRefer',
+        functionName: "verifyRefer",
         args: [address, a, b, c, Input],
       });
       await waitForTransaction({ hash });
       setClaimed(true);
-      toast.success('Claimed Successfully');
+      toast.success("Claimed Successfully");
     } catch (e) {
       console.log(e);
     }
   };
 
   return (
-    <section className='min-h-screen w-screen text-white pt-20 px-10 flex justify-center items-center'>
+    <section className="min-h-screen w-screen text-white pt-20 px-10 flex justify-center items-center">
       <div>
-        <div className='bg-[#151515] w-[500px] p-6 rounded-md'>
+        <div className="bg-[#151515] w-[500px] p-6 rounded-md">
           <div>
             {!claimed ? (
               <div>
-                <p className='text-2xl font-semibold'>Claim your rSIT</p>
-                <p className='text-sm text-gray-400 mt-1'>
+                <p className="text-2xl font-semibold">Claim your rSIT</p>
+                <p className="text-sm text-gray-400 mt-1">
                   Note: Referall system is only available for Indian citizens
                   and the people who have Aadhar card.
                 </p>
 
-                <p className='text-left my-4 font-semibold'>
+                <p className="text-left my-4 font-semibold">
                   10,000 rSITs = 1 SIT
                 </p>
-                {anonAadhaar.status != 'logged-in' ? (
+                {anonAadhaar.status != "logged-in" ? (
                   <LogInWithAnonAadhaar />
                 ) : (
                   <button
                     onClick={verifyReferrer}
-                    className='w-full text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40'>
+                    className="w-full text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40"
+                  >
                     Claim
                   </button>
                 )}
               </div>
             ) : (
-              <div className=''>
-                <p className='text-center text-xl font-semibold mb-4'>
+              <div className="">
+                <p className="text-center text-xl font-semibold mb-4">
                   Successfully Claimed!
                 </p>
-                <p className='text-sm text-gray-300 mb-5'>
+                <p className="text-sm text-gray-300 mb-5">
                   Hurray! You have successfully applied for the rSIT claim. Do
                   the first transaction on platform to claim rSIT.
                 </p>
                 <Image
-                  src='/done.png'
+                  src="/done.png"
                   height={200}
                   width={200}
-                  className='mx-auto'
+                  className="mx-auto"
                 />
 
                 <button
-                  className='w-full text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40 mt-6'
+                  className="w-full text-center py-3 bg-pink-700/40 text-white rounded-md hover:bg-pink-800/40 mt-6"
                   onClick={() => {
-                    router.push('/');
-                  }}>
+                    router.push("/");
+                  }}
+                >
                   Start Investing
                 </button>
               </div>
