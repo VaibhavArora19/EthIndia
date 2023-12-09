bb1dcd2ee31aa73d310c0b044939a8d38c6ddac6
Aman-Mandal <002amankumar@gmail.com>
2023-12-09 18:27:45 +0530

    Fixed onramper styling

--- frontend/components/Modules/Onramper.jsx
+++ frontend/components/Modules/Onramper.jsx
@@ -1,7 +1,7 @@
 const Onramper = () => {
   return (
     <iframe
-      src={`https://buy.onramper.com?themeName=sushi&apiKey=${process.env.NEXT_PUBLIC_ONRAMPER_KEY}&defaultCrypto=ETH&supportRecurringPayments=true`}
+      src={`https://buy.onramper.com?themeName=dark&containerColor=141416&primaryColor=883389&secondaryColor=3f3f43&cardColor=272727&primaryTextColor=ffffff&secondaryTextColor=ffffff&borderRadius=0.54&wgBorderRadius=100apiKey=${process.env.NEXT_PUBLIC_ONRAMPER_KEY}&defaultCrypto=ETH&supportRecurringPayments=true`}
       height='630px'
       width='550px'
       title='Onramper widget'
