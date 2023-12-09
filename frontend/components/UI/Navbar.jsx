412e09278a590e8899e47c95172cd02f6f21bd56
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-12-09 10:07:32 +0530

    added web3modal

--- frontend/components/UI/Navbar.jsx
+++ frontend/components/UI/Navbar.jsx
@@ -1,22 +1,32 @@
-import HomeButton from './HomeButton';
-import { useRouter } from 'next/router';
+import HomeButton from "./HomeButton";
+import { useRouter } from "next/router";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 
 const Navbar = () => {
   const router = useRouter();
+  const { open } = useWeb3Modal();
 
   return (
-    <nav className='w-full py-3 justify-between flex px-10 text-white fixed font-Avenir items-center bg-inherit  z-10'>
+    <nav className="w-full py-3 justify-between flex px-10 text-white fixed font-Avenir items-center bg-inherit  z-10">
       <p>SIT</p>
 
-      <div className='flex gap-4 items-center '>
+      <div className="flex gap-4 items-center ">
         <p
-          className='cursor-pointer'
+          className="cursor-pointer"
           onClick={() => {
-            router.push('/portfolio');
-          }}>
+            router.push("/portfolio");
+          }}
+        >
           Portfolio
         </p>
-        <HomeButton />
+        <button
+          className="bg-[#292929] hover:bg-[#333333] text-sm py-2 px-6 rounded-md"
+          onClick={() => {
+            open();
+          }}
+        >
+          Connect Wallet
+        </button>
       </div>
     </nav>
   );
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,13 +1,16 @@
 import Layout from "@/components/UI/Layout";
 import "@/styles/globals.css";
+import { Web3Modal } from "@/context/Web3Modal";
 import { AnonAadhaarProvider } from "anon-aadhaar-react";
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <AnonAadhaarProvider _appId="196700487049306364386084600156231018794323017728">
-        <Component {...pageProps} />
-      </AnonAadhaarProvider>
-    </Layout>
+    <Web3Modal>
+      <Layout>
+        <AnonAadhaarProvider _appId="196700487049306364386084600156231018794323017728">
+          <Component {...pageProps} />
+        </AnonAadhaarProvider>
+      </Layout>
+    </Web3Modal>
   );
 }
