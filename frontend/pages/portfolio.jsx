84bdca3ab2b39a26b4f62949a231051eed5decdf
Vaibhav Arora <aroravaibhav817@gmail.com>
2023-12-10 01:23:44 +0530

    fixed sit balance

--- frontend/pages/portfolio.jsx
+++ frontend/pages/portfolio.jsx
@@ -1,14 +1,15 @@
-import { getAllData } from '@/developer-apis';
-import React, { useState, useEffect, useCallback } from 'react';
-import Refer from '@/components/Modals/Refer';
-import AssetsHeader from '@/components/Modules/AssetsHeader';
-import AssetsInfo from '@/components/Modules/AssetsInfo';
-import PieChart from '@/components/UI/PieChart';
-import { dummyAssets, headers } from '@/data';
-import Image from 'next/image';
-import { SERVER_URL } from '@/constants';
-import { useAccount } from 'wagmi';
-import { Skeleton } from '@mui/material';
+import { getAllData } from "@/developer-apis";
+import React, { useState, useEffect, useCallback } from "react";
+import Refer from "@/components/Modals/Refer";
+import AssetsHeader from "@/components/Modules/AssetsHeader";
+import AssetsInfo from "@/components/Modules/AssetsInfo";
+import PieChart from "@/components/UI/PieChart";
+import { dummyAssets, headers } from "@/data";
+import Image from "next/image";
+import { SERVER_URL, mainContractABI } from "@/constants";
+import { useAccount } from "wagmi";
+import { Skeleton } from "@mui/material";
+import { ethers } from "ethers";
 
 const Portfolio = () => {
   const [showTable, setShowTable] = useState(false);
@@ -16,14 +17,27 @@ const Portfolio = () => {
   const [showModal, setShowModal] = useState(false);
   const [tokenInfo, setTokenInfo] = useState(null);
   const [totalBalance, setTotalBalance] = useState(null);
+  const [sitBalance, setSitBalance] = useState(null);
   const [roi, setRoi] = useState(null);
   const { address } = useAccount();
 
   const getData = useCallback(async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+    const contract = new ethers.Contract(
+      "0xb1FCFBaf795ad5997e83FC9816BC4BB876ae0399",
+      mainContractABI,
+      provider
+    );
+
+    const balanceSIT = await contract.balanceOf(address);
+
+    setSitBalance(ethers.utils.formatUnits(balanceSIT, 18));
+
     const data = await fetch(`${SERVER_URL}/token/details/${address}`);
 
     const info = await data.json();
-    console.log('gg', info);
+    console.log("gg", info);
     let balance = 0;
     let rateOfInterest = 0;
     for (const token of info.tokenInfo) {
@@ -37,95 +51,111 @@ const Portfolio = () => {
 
   useEffect(() => {
     if (address) {
-      console.log('add', address);
+      console.log("add", address);
       getData();
     }
   }, [address, getData]);
 
   return (
-    <main className='flex flex-col pt-28 min-h-screen  px-10 font-Avenir bg-black w-fit'>
-      <div className='w-fit'>
-        <div className='flex justify-between items-center mb-5'>
-          <p className='text-white text-2xl  font-semibold'>
+    <main className="flex flex-col pt-28 min-h-screen  px-10 font-Avenir bg-black w-fit">
+      <div className="w-fit">
+        <div className="flex justify-between items-center mb-5">
+          <p className="text-white text-2xl  font-semibold">
             {address &&
-              'Welcome, ' +
+              "Welcome, " +
                 address.substring(0, 8) +
-                '...' +
+                "..." +
                 address.substring(36, 43)}
           </p>
           <button
             onClick={() => {
               setShowModal(true);
             }}
-            className='bg-pink-700/40 text-pink-500 hover:bg-pink-800/40 py-3 px-12 text-sm rounded-md cursor-pointer font-semibold'>
+            className="bg-pink-700/40 text-pink-500 hover:bg-pink-800/40 py-3 px-12 text-sm rounded-md cursor-pointer font-semibold"
+          >
             Refer & Earn
           </button>
         </div>
 
-        <div className='flex gap-6'>
-          <div className='bg-gradient-to-tr from-[#4C0D25]  to-[#984253] text-white p-6 rounded-xl w-[350px]'>
+        <div className="flex gap-6">
+          <div className="bg-gradient-to-tr from-[#4C0D25]  to-[#984253] text-white p-6 rounded-xl w-[350px]">
             <Image
               height={35}
               width={35}
-              src='/stats_hovered.svg'
-              alt='Total Earning'
-              className='bg-white/40 rounded-md p-2'
+              src="/stats_hovered.svg"
+              alt="Total Earning"
+              className="bg-white/40 rounded-md p-2"
             />
-            <p className='text-sm mt-8 mb-1'>Total Balance</p>
-            <p className='text-3xl font-semibold'>
+            <p className="text-sm mt-8 mb-1">Total Balance</p>
+            <p className="text-3xl font-semibold">
               {totalBalance ? (
                 `$ ${totalBalance.toString().substring(0, 8)}`
               ) : (
                 <Skeleton
-                  variant='rectangular'
+                  variant="rectangular"
                   width={200}
                   height={40}
                   sx={{
-                    background: '#000/80',
-                    borderRadius: '12px',
-                    marginTop: '8px',
+                    background: "#000/80",
+                    borderRadius: "12px",
+                    marginTop: "8px",
                   }}
                 />
               )}
             </p>
           </div>
 
-          <div className='bg-[#151515] text-[#707070] p-6 rounded-xl w-[350px]'>
+          <div className="bg-[#151515] text-[#707070] p-6 rounded-xl w-[350px]">
             <Image
               height={35}
               width={35}
-              src='/chart.svg'
-              alt='SIT Tokens'
-              className='bg-[#414141] rounded-md p-2'
+              src="/chart.svg"
+              alt="SIT Tokens"
+              className="bg-[#414141] rounded-md p-2"
             />
-            <p className='text-sm mt-8 mb-1'>SIT Tokens</p>
-            <div className='flex items-center justify-between'>
-              <p className='text-3xl font-semibold'>34</p>
-              <p className='text-sm '>~$2,322 USD Coin (POS)</p>
+            <p className="text-sm mt-8 mb-1">SIT Tokens</p>
+            <div className="flex items-center justify-between">
+              <p className="text-3xl font-semibold">
+                {sitBalance ? (
+                  sitBalance
+                ) : (
+                  <Skeleton
+                    variant="rectangular"
+                    width={200}
+                    height={40}
+                    sx={{
+                      background: "#000/80",
+                      borderRadius: "12px",
+                      marginTop: "8px",
+                    }}
+                  />
+                )}
+              </p>
+              <p className="text-sm ">~$2,322 USD Coin (POS)</p>
             </div>
           </div>
 
-          <div className='bg-[#151515] text-[#707070] p-6 rounded-xl w-[350px]'>
+          <div className="bg-[#151515] text-[#707070] p-6 rounded-xl w-[350px]">
             <Image
               height={35}
               width={35}
-              src='/icon.svg'
-              alt='ROI'
-              className='bg-[#414141] rounded-md p-2'
+              src="/icon.svg"
+              alt="ROI"
+              className="bg-[#414141] rounded-md p-2"
             />
-            <p className='text-sm mt-8 mb-1'>Rate Of Interest</p>
-            <p className='text-3xl font-semibold'>
+            <p className="text-sm mt-8 mb-1">Rate Of Interest</p>
+            <p className="text-3xl font-semibold">
               {roi ? (
                 `${Math.round(roi.toString().substring(0, 4) * 100)}%`
               ) : (
                 <Skeleton
-                  variant='rectangular'
+                  variant="rectangular"
                   width={200}
                   height={40}
                   sx={{
-                    background: '#000/80',
-                    borderRadius: '12px',
-                    marginTop: '8px',
+                    background: "#000/80",
+                    borderRadius: "12px",
+                    marginTop: "8px",
                   }}
                 />
               )}
@@ -135,7 +165,7 @@ const Portfolio = () => {
       </div>
 
       <div>
-        <div className='flex mb-5 gap-2 mt-10'>
+        <div className="flex mb-5 gap-2 mt-10">
           <p
             onClick={() => {
               setShowTable(true);
@@ -143,9 +173,10 @@ const Portfolio = () => {
             }}
             className={`${
               showTable
-                ? 'bg-pink-700/20 text-pink-500 hover:bg-pink-700/40'
-                : 'text-gray-400'
-            }  hover:bg-[#2A2B2E] py-1 px-12 text-sm rounded-md cursor-pointer`}>
+                ? "bg-pink-700/20 text-pink-500 hover:bg-pink-700/40"
+                : "text-gray-400"
+            }  hover:bg-[#2A2B2E] py-1 px-12 text-sm rounded-md cursor-pointer`}
+          >
             Assets
           </p>
           <p
@@ -155,9 +186,10 @@ const Portfolio = () => {
             }}
             className={`${
               showChart
-                ? 'bg-pink-700/20 text-pink-500 hover:bg-pink-700/40'
-                : 'text-gray-400'
-            }  hover:bg-[#2A2B2E] py-1 text-sm px-12 rounded-md cursor-pointer`}>
+                ? "bg-pink-700/20 text-pink-500 hover:bg-pink-700/40"
+                : "text-gray-400"
+            }  hover:bg-[#2A2B2E] py-1 text-sm px-12 rounded-md cursor-pointer`}
+          >
             Chart
           </p>
         </div>
@@ -165,25 +197,24 @@ const Portfolio = () => {
 
       {showChart &&
         (tokenInfo ? (
-          <div className='w-[800px] h-[800px]  mt-10 mx-auto'>
+          <div className="w-[800px] h-[800px]  mt-10 mx-auto">
             <PieChart tokenInfo={tokenInfo} />
           </div>
         ) : (
           <Skeleton
-            variant='rectangular'
+            variant="rectangular"
             width={500}
             height={500}
             sx={{
-              background: '#1f1f1f',
-              borderRadius: '500px',
-              margin: '8px auto',
-
+              background: "#1f1f1f",
+              borderRadius: "500px",
+              margin: "8px auto",
             }}
           />
         ))}
 
       {showTable && (
-        <div className='w-full'>
+        <div className="w-full">
           <AssetsHeader headers={headers} />
 
           {tokenInfo &&
